e962640f3b2b7cd082cf54ddf9b95a55163d2b0c
D S Keshav <dkeshav@BLR.ALLEGISINDIA.COM>
2018-10-23 19:55:06 +0530

    modifying and reordering Adult model

--- python_programming_exercise/udemy_projects/Adult/main.py
+++ python_programming_exercise/udemy_projects/Adult/main.py
@@ -6,8 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
 
-#write to adult.data file if empty
+#get training dataset 
 data_url="https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
 names=['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex',
         'capital-gain','capital-loss','hours-per-week','native-country','class']
@@ -16,21 +17,37 @@ dataset_data = pd.read_csv(data_url, names=names)
 print(type(dataset_data))
 print("dataset_data shape: ",dataset_data.shape)
 print(dataset_data.describe())
-print(dataset_data.groupby('class').size())
 
-#write to adult.test if empty
+#Raplace missing values ? with NaN
+dataset_data[names]=dataset_data[names].replace(' ?',np.nan)
+
+#Drop rows which contains NaN
+dataset_data=dataset_data.dropna()
+print("dataset_data shape after row removal:\n",dataset_data.shape)
+print("dataset_data class:\n",dataset_data.groupby('class').size())
+
+# Get test dataset
 test_url="https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test"
 dataset_test=pd.read_csv(test_url,names=names)
 print("dataset_test shape: ",dataset_test.shape)
 #drop the first index of test dataset due to invalid data  
 dataset_test.drop(dataset_test.index[0], inplace=True)
-print("dataset_test shape: ",dataset_test.shape)
+# print("dataset_test shape: ",dataset_test.shape)
 
+
+#Raplace missing values ? with NaN
+dataset_test[names]=dataset_test[names].replace(' ?',np.nan)
+dataset_test=dataset_test.dropna()
+print('dataset_test shape after removal: ',dataset_test.shape)
+
+#strip the period from the class column to maintain consistency with the training dataset 
+dataset_test['class']=dataset_test['class'].str.strip('.')
+print("dataset_test class:\n",dataset_test.groupby('class').size())
 #target variable
 target="class"
+
 #Get all the columns from the dataframe
 columns= dataset_data.columns.tolist()
-
 
 #filter the columns to remove data we do not want
 columns=[c for c in columns if c not in ['class','native-country','sex','race','relationship','education','occupation','marital-status','workclass']]
@@ -38,10 +55,14 @@ print("columns :",columns)
 
 
 #classifier algorithm
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier  #
+from sklearn.naive_bayes import BernoulliNB,GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report ,confusion_matrix , accuracy_score
+
+#time interval
+import time
 
 #10 -fold Cross Validation dataset
 seed=7
@@ -49,9 +70,13 @@ scoring='accuracy'
 
 #models
 models=[]
-models.append(('DTC',DecisionTreeClassifier()))
+models.append(('CART',DecisionTreeClassifier()))
+models.append(('NB',GaussianNB()))
 models.append(('BNB',BernoulliNB()))
-models.append(('KNN',KNeighborsClassifier()))
+models.append(('KNN1',KNeighborsClassifier(n_neighbors=1)))
+models.append(('KNN3',KNeighborsClassifier(n_neighbors=3)))
+models.append(('SVM',SVC()))
+
 
 results=[]
 names=[]
@@ -66,42 +91,30 @@ for name,model in models:
 
 
 for name, model in models:
+#     t1=time()
     model.fit(dataset_data[columns], dataset_data['class'])
-    print("dataset_test[columns]:\n",dataset_test[columns])
+#     t2=time()-t1
     predictions = model.predict(dataset_test[columns])
+#     if(name=='SVC'):
+#             with open('adult.test.csv','r') as filepred:
+#                 for wrap in predictions:
+#                         filepred.writeline(wrap)
+    print("type of predictions: ",type(predictions))
+    print("predict: ",predictions.size," ",predictions)
     print(name)
     print(accuracy_score(dataset_test['class'], predictions))
     print(classification_report(dataset_test['class'], predictions))
-
-
-#     model.fit(X_train, y_train)
-   
-#     predictions = model.predict(X_validation)
-#     print(name)
-#     print(accuracy_score(y_validation, predictions))
-#     print(classification_report(y_validation, predictions))
-# dataset_data=dataset_data[dataset_data.names !=' ?']
-
-# dataset_data[].replace(' ?',pd.np.NaN)
-# print("after replace: ",dataset_data.shape)
-# print(dataset_data[dataset_data==pd.np.NaN].iloc[0])
-# dataset_data.dropna(inplace=True)
-# print("After drop: ",dataset_data.shape)
-# pd.plotting.scatter_matrix(dataset_data,c=names,figsize=[8,8],s=150,marker='D')
-
-
-
+#     print("trining time of {}: ".format(model),round(t2,3))
 #histogram representation of dataset
 # dataset_data.hist()
 # plt.show()
-
-
 
 # #Corelation matrix
 # corrmat=dataset_data.corr()
 # fig=plt.figure(figsize=(12,9))
 # sns.heatmap(corrmat,vmax=1,square=True)
 # plt.show()
+
 # # response=requests.get(data_url)
 # # with open("adult.data.csv",'w') as datacsv:
 # #     if response.status_code != 200:
@@ -112,11 +125,21 @@ for name, model in models:
 # #         csv
 
 
-# # plt.hist(dataset_test['capital-gain'])
-# # plt.show()
 
 # # #Corelation matrix
 # # corrmat=dataset_test.corr()
 # # fig=plt.figure(figsize=(12,9))
 # # sns.heatmap(corrmat,vmax=.8,square=True)
 # # plt.show()
+
+
+
+# data_url="https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+# names=['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex',
+#         'capital-gain','capital-loss','hours-per-week','native-country','class']
+# #write to adult.data file if empty
+# if (os.path.getsize("adult.data.csv")==0):
+#        with open("adult.data.csv",'w') as traindatafile:
+#                traindatafile.write(','.join(names))
+# else:
+#         print("empty file")
