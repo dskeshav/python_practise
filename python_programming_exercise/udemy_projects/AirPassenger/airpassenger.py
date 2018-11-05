e64c081c526bad71a034e74d578a5680b0fdb16c
D S Keshav <dkeshav@BLR.ALLEGISINDIA.COM>
2018-11-05 19:23:28 +0530

    minor changes

--- python_programming_exercise/udemy_projects/AirPassenger/airpassenger.py
+++ python_programming_exercise/udemy_projects/AirPassenger/airpassenger.py
@@ -33,11 +33,41 @@ print('entire range:\n',ts['1949-01-01':'1949-05-01'])
 print('indices ends: ',ts[:'1949-05-01'])
 
 print('1949:\n',ts['1949'])
-
+plt.hist(ts)
+plt.show(block=Fa)
 plt.plot(ts)
+plt.show()
 print("print")
 
 from statsmodels.tsa.stattools import adfuller
 def test_stationarity(timeseries):
-#Determing rolling statistics
-    rolmean=pd.rolling_mean(timeseries,window=12)
+    #Determing rolling statistics
+    rolmean=timeseries.rolling(window=12).mean()
+    rolstd=timeseries.rolling(window=12).std()
+
+    #plot rolling statistics:
+    orgi=plt.plot(timeseries,color='blue',label='Original')
+    mean=plt.plot(rolmean,color='red',label='Rolling Mean')
+    std=plt.plot(rolstd,color='black',label='Rolling std')
+
+    plt.legend(loc='best')
+    plt.title('Rolling Mean and Standard Deviation')
+    
+    df = pd.DataFrame(index=ts.index, columns=[orgi,mean,std])
+    df = df.cumsum()
+    plt.figure()
+    df.plot()
+    plt.show()
+
+    #perform Dickey-Fuller test:
+    print('Result of Dickey-Fuller Test:')
+    dftest=adfuller(timeseries,autolag='AIC')
+    dfoutput=pd.Series(dftest[0:4],index=['Test Statistics','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key]=value
+    print(dfoutput)
+
+#Test Stationarity for time series dataset
+test_stationarity(ts)
+
+
